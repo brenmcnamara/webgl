518d34ac41dbdd3c3e6f6a8098b7cea5f45d2621
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-04-04 15:57:47 -0700

    Adds font-awesome -- closes #9

--- src/__tests__/index-tests.js
+++ src/__tests__/index-tests.js
@@ -1,6 +1,4 @@
 
-import app from '..';
-
 test('passes', () => {
   expect(2 + 2).toBe(4);
 });
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ window.onload = function() {
     <div>
       <h1>Starter Kit</h1>
       <p>Hello From Starter Kit</p>
+      <i className="fa fa-fort-awesome" aria-hidden="true"></i>
     </div>,
     document.getElementById('root')
   );
