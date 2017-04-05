763104b3eb428030e28b61ecdac9df2c6ddd1744
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-04-04 17:40:07 -0700

    Adds browser sync server -- closes #11

--- buildScripts/startServer.js
+++ buildScripts/startServer.js
@@ -1,23 +1,7 @@
 /* eslint-disable no-console */
 
-import express from 'express';
-import path from 'path';
-import compression from 'compression';
+import browserSync from 'browser-sync';
 
-const port = 3000;
-const app = express();
+const server = browserSync.create();
 
-app.use(express.static('dist'));
-app.use(compression());
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('listening on port ' + port);
-  }
-});
+server.init({ server: './dist' });
--- src/index.js
+++ src/index.js
@@ -7,8 +7,33 @@ window.onload = function() {
     <div>
       <h1>Starter Kit</h1>
       <p>Hello From Starter Kit</p>
-      <i className="fa fa-fort-awesome" aria-hidden="true"></i>
+      <LoremIpsum />
     </div>,
     document.getElementById('root')
   );
+}
+
+class LoremIpsum extends React.Component {
+
+  render() {
+    const elems = [];
+    for (let i = 0; i < 20; ++i) {
+      elems.push(this._renderLoremIpsum(i));
+    }
+    return <div>{ elems }</div>
+  }
+
+  _renderLoremIpsum(i) {
+    return (
+      <p className="pt-running-text" key={i}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+        est laborum
+      </p>
+    );
+  }
+
 }
