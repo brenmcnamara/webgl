fbbec0a3a191cb86eee4f5a0ee9332c4e8c8e7bd
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-04-04 20:54:25 -0700

    Adds flow to project

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Starter Kit for web apps running with Better Note.
 - Install [node](https://nodejs.org/en/)
 - Install [nvm](https://github.com/creationix/nvm)
 - Install [yarn](https://yarnpkg.com/en/docs/install)
+- Install [flow](https://github.com/facebook/flow)
+
+## Setup (Highly Recommended)
+
+- Install [atom](https://atom.io/)
+- Install [nuclide](https://nuclide.io/docs/quick-start/getting-started/#installation)
 
 ## Setup (Repo)
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* @flow */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,33 +8,7 @@ window.onload = function() {
     <div>
       <h1>Starter Kit</h1>
       <p>Hello From Starter Kit</p>
-      <LoremIpsum />
     </div>,
     document.getElementById('root')
   );
-}
-
-class LoremIpsum extends React.Component {
-
-  render() {
-    const elems = [];
-    for (let i = 0; i < 20; ++i) {
-      elems.push(this._renderLoremIpsum(i));
-    }
-    return <div>{ elems }</div>
-  }
-
-  _renderLoremIpsum(i) {
-    return (
-      <p className="pt-running-text" key={i}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-        est laborum
-      </p>
-    );
-  }
-
 }
